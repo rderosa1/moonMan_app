96e87806fc15828b604e128a4d65c2917370bb0d
marykellar <marymac@maryxmac.com>
2020-03-11 13:14:47 -0400

    pulled down latest and edited client/src/screens/Items.jsx with new search feature

--- client/src/screens/Items.jsx
+++ client/src/screens/Items.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Layout from '../components/shared/Layout'
-import { getItemById, updateItem } from '../services/items'
+import { getItems, getItemById, updateItem } from '../services/items'
 
-export default function Items(props) {
-  const { history, match, user, items } = props
-  const renderButton = id => {
+class Items extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      search: '',
+      searchresult: null
+    }
+  }
+
+  renderButton = (id) => {
+    const { history, match, user } = this.props
     if (user) {
       return (
         <button onClick={() => history.push(`${match.url}/${id}`)}>
@@ -16,13 +24,14 @@ export default function Items(props) {
     }
   }
 
-  const renderItems = () => {
+  renderItems = () => {
+    const { items } = this.props
     if (items) {
       return items.map(item => {
         return (
           <div className="item" key={item._id}>
             <h4>{item.title}</h4>
-            {renderButton(item._id)}
+            {this.renderButton(item._id)}
           </div>
         )
       })
@@ -31,53 +40,81 @@ export default function Items(props) {
     }
   }
 
-  const handleSubmit = event => {
+  handleChange = event => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault()
+    const { items } = this.props
+    console.log('submitted')
     if (items) {
-      
-      return items.filter(item => {
-        return (
-          <div>
-            <h4>{item.title}</h4>
-            {renderButton(item._id)}
-          </div>
-        )
+
+      const result = items.filter(item => {
+        return item.title === this.state.search
       })
-      
+      console.log(result)
+      this.setState({
+        searchresult: result
+      })
     }
   }
 
-  if (user) {
-    return (
-      <Layout>
-        <h4>Items</h4>
-        <form>
-          <input className="input"
-            type="text"
-            placeholder="search by title"
-          // value={props.value}
-          // onChange={handleChange}
-          />
-          <button
-            type="submit"
-            onClick={handleSubmit}
-          //handleSubmit could filter.
-          >search</button>
-        </form>
-        <h1>searched items</h1>
-        {!items ? <h3>No Items at this time.</h3> : null}
-        <div className="item-container">{renderItems()}</div>
-      </Layout>
-    )
-  } else {
-    return (
-      <div className="landing">
-        <h2>Welcome to the Items App!</h2>
-        <div className="main">
-          {!items ? <h3>No Items at this time.</h3> : null}
-          <div className="item-container">{renderItems()}</div>
+  render() {
+    // const { history, match, user, items } = props
+    const { searchresult } = this.state
+    if (searchresult) {
+      return (
+        <div>
+          {this.state.searchresult.map((result) => {
+            return(
+              <div>
+                <h1>{result.title}</h1>
+                <h2>{result.link}</h2>
+                {this.renderButton(result._id)}
+              </div>
+            )
+          })}
+      
         </div>
-      </div>
-    )
+      )
+    }
+    else {
+      const { user, items } = this.props
+      if (user) {
+        return (
+          < Layout >
+            <h4>Items</h4>
+            <form onSubmit={this.handleSubmit}>
+              <input className="input"
+                type="text"
+                placeholder="search by title"
+                // value={this.state.search}
+                onChange={this.handleChange}
+              />
+              <button
+                type="submit"
+              >search</button>
+            </form>
+            {!items ? <h3>No Items at this time.</h3> : null}
+            <div className="item-container">{this.renderItems()}</div>
+          </Layout >
+        )
+      } else
+        return (
+          <div className="landing">
+            <h2>Welcome to the Items App!</h2>
+            <div className="main">
+              {!items ? <h3>No Items at this time.</h3> : null}
+              <div className="item-container">{this.renderItems()}</div>
+            </div>
+          </div>
+        )
+    }
   }
 }
+
+
+export default Items
