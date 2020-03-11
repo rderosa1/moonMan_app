d836bbe6c1dc86417b89b628bf1b9465152360be
Jonathanh7 <Jonathanh949@gmail.com>
2020-03-10 22:12:22 -0400

    homepage adjustment

--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = (props) => (
   <div className='container-landing'>
     <h1>Would You Like To Develop An App?</h1>
        {/* <Items {...props} />    */}
-    <img className="aesthetic" src="https://i.imgur.com/R6RIwtQh.jpg" />
+    {/* <img className="aesthetic" src="https://i.imgur.com/R6RIwtQh.jpg" /> */}
     </div>
 )
 export default Landing
