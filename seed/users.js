e22bddfa19b643cd197b190bf499fa988addae04
ro <r.cosenza39@gmail.com>
2020-03-12 11:07:42 -0400

    mugh

--- seed/users.js
+++ seed/users.js
@@ -1,19 +1,21 @@
 const db = require('../db')
 const User = require('../models/user')
+const Item = require('../models/item')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-const faker = require('faker')
+//const faker = require('faker')
 
 const main = async () => {
-  const users = [...Array(100)].map(user => (
-    {
-      username: faker.name.firstName(),
-      email: faker.internet.email(),
-      password_digest: faker.random.uuid(),
-      items: {}
-    }
-  ))
+  const bike = await Item.find({ title: 'bike' })
+  const shoe = await Item.find({ title: 'shoe' })
+  console.log(bike)
+
+  const users = [
+    { username: "rico", email: "ricoderosa@yaho.com", password_digest: "password", items: [bike[0]._id, shoe[0]._id] },
+    { username: "elmo", email: "rgsdhsdha@yaho.com", password_digest: "pass" },
+
+  ]
 
   await User.insertMany(users)
   console.log("Created users!")
