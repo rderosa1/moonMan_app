9ffefeea059313115a68b03e64ee3a6919b4704a
Jonathanh7 <Jonathanh949@gmail.com>
2020-03-12 10:14:05 -0400

    changes background image defects

--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -4,9 +4,9 @@ import Items from './Items'
 const Landing = (props) => (
 <div className='container-landing'>
       {/* <Items {...props} />    */}
-    <div>
+    
       <h1 className="showcase">moonMen</h1>
-    </div>
+    
     </div>
 )
 export default Landing
