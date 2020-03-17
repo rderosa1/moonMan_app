9646c292895fd04355fb48396c348805fb76024b
Jonathanh7 <Jonathanh949@gmail.com>
2020-03-16 20:12:53 -0400

    Dark Mode add, more edits to come later today

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "./components/Container";
 import "./styles/App.css";
 import Particles from "react-particles-js";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+.container-landing {
+  background-color: ${props => (props.theme.mode === "dark" ? "#111" : "transparent")};
+  background-image: ${props => (props.theme.mode === "dark" ? 'url(".AdobeStock_105677222.jpeg")' : "#111")};
+}
+`;
 
 export default function App() {
+  const [theme, setTheme] = useState({ mode: "light" })
   return (
     <>
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 400,
-              density: {
-                enable: false
-              }
-            },
-            color: {
-              value: ["#AAAAAA","#DDDDDD", "#111111"]
-            },
-            size: {
-              value: 4,
-              random: true,
-              anim: {
-                speed: 4,
-                size_min: 0.3
-              }
-            },
-            line_linked: {
-              enable: false
-            },
-            move: {
-              random: true,
-              speed: 1,
-              direction: "top",
-              out_mode: "out"
-            }
-          },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: "bubble"
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Particles
+            params={{
+              particles: {
+                number: {
+                  value: 400,
+                  density: {
+                    enable: false
+                  }
+                },
+                color: {
+                  value: ["#AAAAAA", "#DDDDDD", "#111111"]
+                },
+                size: {
+                  value: 4,
+                  random: true,
+                  anim: {
+                    speed: 4,
+                    size_min: 0.3
+                  }
+                },
+                line_linked: {
+                  enable: false
+                },
+                move: {
+                  random: true,
+                  speed: 1,
+                  direction: "top",
+                  out_mode: "out"
+                }
               },
-              onclick: {
-                enable: true,
-                mode: "repulse"
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "bubble"
+                  },
+                  onclick: {
+                    enable: true,
+                    mode: "repulse"
+                  }
+                },
+                modes: {
+                  bubble: {
+                    distance: 250,
+                    duration: 2,
+                    size: 0,
+                    opacity: 0
+                  },
+                  repulse: {
+                    distance: 400,
+                    duration: 4
+                  }
+                }
               }
-            },
-            modes: {
-              bubble: {
-                distance: 250,
-                duration: 2,
-                size: 0,
-                opacity: 0
-              },
-              repulse: {
-                distance: 400,
-                duration: 4
-              }
+            }}
+            className="particles"
+          />
+          <Container theme={theme} setTheme={setTheme} />
+
+          {/* <button className="dark-mode"
+            onClick={e => 
+              setTheme(
+                theme.mode === 'dark'
+                  ? { mode: 'light' }
+                  : { mode: 'dark' }
+            )
             }
-          }
-        }}
-        className="particles"
-      />
-      <Container />
+            >Dark Mode</button> */}
+        </>
+      </ThemeProvider>
     </>
   );
 }
